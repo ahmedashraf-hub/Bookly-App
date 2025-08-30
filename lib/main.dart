92352627a94c39df9560f3bd7d3fc0dbc13cac71
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-08-30 21:27:45 +0300

    build Structure and Create Splash.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(home: SplashView());
+  }
+}
