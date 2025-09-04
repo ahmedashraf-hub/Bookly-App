17744e1b4f3f827460e71d7c567ef94b2a5ddc29
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-09-04 16:23:00 +0300

    Best Seller item part thrree

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -25,15 +26,29 @@ class BestSellerListViewItem extends StatelessWidget {
           ),
           const SizedBox(width: 30),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
-                child: const Text(
-                  'Harry Potter\nand the Goblet of Fire',
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
                 ),
+              ),
+              const SizedBox(height: 3),
+              const Text('J.K. Rowling', style: Styles.textStyle14),
+              const SizedBox(height: 3),
+              Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
