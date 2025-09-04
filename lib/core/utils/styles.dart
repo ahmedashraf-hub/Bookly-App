68117154c28cc6b6948eb458f5ef1468e940566a
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-09-04 15:58:15 +0300

    Finish App Styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -11,4 +11,17 @@ abstract class Styles {
     fontWeight: FontWeight.normal,
     fontFamily: kGtSectraFine,
   );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
 }
