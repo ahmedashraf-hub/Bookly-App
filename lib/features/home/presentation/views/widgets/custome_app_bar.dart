80cbe6e0ab078b2ef1a3598d642e05364577a390
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-09-05 04:53:00 +0300

    Create custom serch text field

--- lib/features/home/presentation/views/widgets/custome_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custome_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomeAppBar extends StatelessWidget {
   const CustomeAppBar({super.key});
@@ -14,8 +16,10 @@ class CustomeAppBar extends StatelessWidget {
           Image.asset(AssetsData.logo, height: 18),
           Spacer(),
           IconButton(
-            onPressed: () {},
-            icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 24),
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 22),
           ),
         ],
       ),
