b6250f2240655856ff1f30c35f07dcd0b4a66b3c
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-09-08 20:17:23 +0300

    Book details done

---
+++ lib/core/utils/functions/custom_snackbar.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void customSnackbar(context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+}
--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,7 +1,7 @@
+import 'package:bookly/core/utils/functions/launch_url.dart';
 import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
@@ -27,16 +27,13 @@ class BooksAction extends StatelessWidget {
           Expanded(
             child: CustomButton(
               onPressed: () async {
-                Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
-                if (await canLaunchUrl(uri)) {
-                  await launchUrl(uri);
-                }
+                launchCustomUrl(context, bookModel.volumeInfo.previewLink!);
               },
               fontSize: 16,
-              title: 'Free preview',
+              title: getText(bookModel),
               backgroundColor: Color(0XFFEF8262),
               textColor: Colors.white,
-              borderRadius: BorderRadius.only(
+              borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(16.0),
                 bottomRight: Radius.circular(16.0),
               ),
@@ -45,5 +42,13 @@ class BooksAction extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (!(bookModel.volumeInfo.previewLink == null)) {
+      return 'Not Available';
+    } else {
+      return 'Preview';
+    }
   }
 }
